1c1ccbfd9ad26f1f3f45353acf4f114586766202
tatendabakozw <tatendabakozw@gmail.com>
2021-01-05 12:40:56 +0200

    database initalisation

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
-const mongoose = require('mongoose')
-const mongoUrl = process.env.mongoUrl
-const mongoOptions = {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
+const Pool = require('pg').Pool
 
-const connectDB = () =>{
-    mongoose.connect(mongoUrl, mongoOptions)
-    mongoose.connection.once('open',(err)=>{
-        if(err){
-            console.log(err);
-        }else{
-            console.log('Database connected successfully');
-        }
-    })
-}
+const pool = new Pool({
+    host: process.env.host,
+    port: process.env.db_port,
+    password: process.env.password,
+    database:process.env.database,
+    user: process.env.user
+})
 
-module.exports = connectDB
+module.exports = pool
--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require('express')
 const morgan = require('morgan')
-const helmet = require('helmet')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express')
 const app = express()
+require('dotenv').config()
+const pool = require('./db')
 
+//connecting database
+pool.connect((error)=>{
+    if(error){
+        console.log(error.message)
+    }else{
+       console.log("database connectsed successfully") 
+    }
+})
 //app level middleware
-app.use(helmet())
 app.use(morgan('common'))
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-//connecting database
-const connectDB = require('./db')
-connectDB()
-
 //user defined routes
-const channelRoute = require('./routes/conversation')
-app.use('/api/v1', channelRoute)
+const userRoute = require('./routes/user')
+app.use('/api/v1',userRoute )
 
 //listener
-const port = process.env.port || 8080
-app.listen(port, (err)=>{
-    if(err){
-        console.log(err)
+const port = process.env.port || 5500
+
+app.listen(port,(error)=>{
+    if(error){
+        console.log(error.message)
     }else{
-        console.log(`server up on port ${port}`)
+        console.log(`Server up on port ${port}`)
     }
 })
